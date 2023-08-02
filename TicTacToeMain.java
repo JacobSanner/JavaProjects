a8ef89ad91fb7ba53ddabc7d0ad9b0b6e54016aa
jacob <jacob.t.sanner2@gmail.com>
2023-08-02 09:31:53 -0700

    Initial commit

---
+++ TicTacToeMain.java
@@ -0,0 +1,17 @@
+public class TicTacToeMain {
+    public static void main(String[] args) {
+        TicTactToe game = new TicTactToe();
+
+        //JOptionPane.showMessageDialog(null, "Hello World");
+       // JFrame myFrame = new JFrame(null, null);
+        //myFrame.setSize(800, 800);
+       // myFrame.setLocation(100, 100);
+       // myFrame.setVisible(true);
+        //myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //JPanel myPanel = new JPanel(null, true);
+        
+
+
+    }
+}
